6d8e7576473a13da4805cb10271fdc0edb9bc360
suhail ansar <125738472+suhailanzar@users.noreply.github.com>
2024-03-26 15:39:26 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,46 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
-  title = 'Todolist';
+  todolist: string[] = [];
+  newtask: string = '';
+  finishedtask: string[] = [];
+  sametask:string = ''
+
+  addTodo() {
+    if (this.newtask.trim() !== '') {
+      const taskExists = this.todolist.includes(this.newtask);
+      if (taskExists) {
+        this.sametask = 'Task already exists!';
+        setTimeout(() => {
+         this.sametask = '';
+        }, 2000);
+       
+      } else {
+        this.todolist.push(this.newtask);
+        this.newtask = '';
+        this.sametask = ''; 
+      }
+    }
+  }
+  
+
+
+  removeTodo(index: number) {
+    this.todolist.splice(index, 1);
+  }
+
+  completedtodo(index:number){
+
+    this.finishedtask.push(this.todolist[index])
+    this.todolist.splice(index, 1);
+
+
+  }
+
+  removeTodoFromFinished(index:number){
+    this.finishedtask.splice(index,1)
+  }
 }
